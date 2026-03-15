4a32eff00ff2b1bfbc338c8330c3e47aee88d9fe
IntooyaAPP <founder@sitruclabs.com>
2026-03-14 22:25:31 -0700

    Create handler.py

---
+++ handler.py
@@ -0,0 +1,21 @@
+import runpod
+
+def handler(job):
+    job_input = job["input"]
+
+    script = job_input.get("script")
+    avatar_video_url = job_input.get("avatar_video_url")
+    voice_sample_url = job_input.get("voice_sample_url")
+
+    print("Received job:")
+    print(script)
+    print(avatar_video_url)
+    print(voice_sample_url)
+
+    # temporary test response
+    return {
+        "status": "worker running",
+        "script": script
+    }
+
+runpod.serverless.start({"handler": handler})
