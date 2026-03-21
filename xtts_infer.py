4a9805d731a311ddaf444b54a61795661ee5209c
IntooyaAPP <founder@sitruclabs.com>
2026-03-20 20:46:53 -0700

    Update xtts_infer.py

--- xtts_infer.py
+++ xtts_infer.py
@@ -1,5 +1,8 @@
 import sys
 from TTS.api import TTS
+import os
+
+os.environ["COQUI_TOS_AGREED"] = "1"
 
 if len(sys.argv) != 4:
     print("usage: python xtts_infer.py <text> <speaker_wav> <output_wav>")
@@ -9,13 +12,14 @@ text = sys.argv[1]
 speaker_wav = sys.argv[2]
 output_wav = sys.argv[3]
 
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2")
+# Use pre-cached models from network volume
+os.environ["TTS_HOME"] = "/runpod-volume"
 
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2")
 tts.tts_to_file(
     text=text,
     speaker_wav=speaker_wav,
     language="en",
     file_path=output_wav
 )
-
 print(output_wav)
